eb3b296cce9f098fc9d5eaee07d1c0c9439c7b44
brittanyaylia <whitakerbrittany18@gmail.com>
2021-05-13 19:29:41 -0700

    code

--- static/js/app.js
+++ static/js/app.js
@@ -2,6 +2,22 @@
 
 // from data.js
 var tableData = data;
-// table body declaration 
+// table body selection 
 let tbody = d3.select("tbody");
 
+//build table function
+function buildTable(data){
+    //clear present data
+    tbody.html("");
+    //loop through data
+    data.forEach((dataRow) => {
+        //append table row to table body 
+        let row = tbody.append("tr");
+        //iterate through data using object.values & foreach 
+        Object.values(dataRow).forEach((val) => {
+            //append cell to row 
+            let cell = row.append("td");
+            cell.text(val);
+        });
+    })
+}
